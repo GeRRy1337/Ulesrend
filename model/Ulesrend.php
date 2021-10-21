c8148ac7a1448bdb5cf9c2233e88839d02661a66
GeRRy1337 <mogema2002@gmail.com>
2021-10-21 10:58:03 +0200

    set,get user

--- model/Ulesrend.php
+++ model/Ulesrend.php
@@ -1,5 +1,6 @@
-<?php 
-    require 'db.inc.php';
+<?php
+    require '../db.inc.php';
+
     class Ulesrend{
         private $id;
         private $nev;
@@ -8,15 +9,27 @@
         private $jelszo;
         private $felhasznalonev;
 
-        public function get_user($id){
-            // adatbázisból lekérdezzük
+        public function set_user($id,$conn){
             $sql = "SELECT * FROM `5/13ice` WHERE id=$id";
 		    $result = $conn->query($sql);
             if ($result->num_rows > 0) {
-                $row = $result->fetch_assoc()
-                return $row;
+                $row = $result->fetch_assoc();
+                $this->id = $row["id"];
+                $this->nev = $row["nev"];
+                $this->sor = $row["sor"];
+                $this->oszlop = $row["oszlop"];
+                $this->jelszo = $row["jelszo"];
+                $this->felhasznalonev = $row["felhasznalonev"];
+
             }
-            else return FALSE;   
         }
-    }
+
+        public function get_nev(){
+           return $this->nev;
+        }
+    }   
+    $tanulo = new Ulesrend;
+    $tanulo ->set_user(2, $conn);
+    echo $tanulo->get_nev();
+
 ?>
