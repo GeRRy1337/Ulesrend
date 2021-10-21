7b291ad7df30cef231c45171828d8cd29f178224
GeRRy1337 <mogema2002@gmail.com>
2021-10-21 10:21:08 +0200

    Ulesrend Objektum létrehozása

---
+++ model/Ulesrend.php
@@ -0,0 +1,18 @@
+<?php 
+    class Ulesrend{
+        private $id;
+        private $nev;
+        private $sor;
+        private $oszlop;
+        private $jelszo;
+        private $felhasznalonev;
+
+        public function get_user($id){
+            // adatbázisból lekérdezzük
+            $sql = "SELECT * FROM `5/13ice` WHERE id=".$id;
+		    $result = $conn->query($sql);
+            //visszaadjuk
+            return $result;
+        }
+    }
+?>
